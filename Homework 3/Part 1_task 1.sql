ec45143db6c60f8a9d7f23585725a36195db970d
Maksim Filimonenkau <maxfilimonenkov@yahoo.com>
2026-08-19 17:31:26 +0300

    Часть 1 - Where

---
+++ Homework 3/Part 1_task 1.sql
@@ -0,0 +1,6 @@
+SELECT 
+    first_name, last_name, age, country
+FROM 
+    customers
+WHERE 
+    country='USA'
---
+++ Homework 3/Part 1_task 2.sql
@@ -0,0 +1,6 @@
+SELECT 
+    order_id, item, amount, customer_id
+FROM 
+    orders
+WHERE 
+    amount > 1000
