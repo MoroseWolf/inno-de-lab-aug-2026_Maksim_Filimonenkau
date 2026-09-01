f12c78c09768f60813a3ea70d76e64e325d444df
Maksim Filimonenkau <maxfilimonenkov@yahoo.com>
2026-09-01 22:28:23 +0300

    Задание 2

--- Homework 7/tasks.py
+++ Homework 7/tasks.py
@@ -1,7 +1,7 @@
 # Задача 1: Нормализация и сборка записи пользователя
 
 raw_user_record = " 10827 ; aLeXanDer_vLaDimiRov ; mInSk ; ACTIVE "
-
+raw_transactions = ["SUCCESS:100", "FAILED:50", "SUCCESS:-10", "SUCCESS:0", "SUCCESS:250", "ERROR:200"] 
 
 def parse_task_1(user_record): 
     raw_parts = user_record.split(";")
@@ -22,3 +22,10 @@ def parse_task_1(user_record):
 
 task_1 = parse_task_1(raw_user_record)
 print(task_1)
+
+task_2 = [
+    int(pair.split(":")[1])
+    for pair in raw_transactions
+    if pair.split(":")[0] == "SUCCESS" and int(pair.split(":")[1]) > 0
+    ]
+print(task_2)
